c91b7b9ef6ae61a055dd6884e87f765a07afd2cf
Douglas Lowder <douglowder@mac.com>
2025-04-01 20:17:39 -0700

    Implement scoped presentation module

--- apps/expo-go/ios/Exponent/Versioned/Core/Modules/Notifications/ExpoGoNotificationsPresentationModule.swift
+++ apps/expo-go/ios/Exponent/Versioned/Core/Modules/Notifications/ExpoGoNotificationsPresentationModule.swift
@@ -15,4 +15,45 @@ public final class ExpoGoNotificationsPresentationModule: PresentationModule {
 
     super.init(appContext: appContext)
   }
+
+  public override func serializeNotifications(_ notifications: [UNNotification]) -> [[String: Any]] {
+    return notifications
+      .filter { notification in
+        EXScopedNotificationsUtils.shouldNotification(notification, beHandledByExperience: self.scopeKey)
+      }
+      .map { notification in
+        return EXScopedNotificationSerializer.serializedNotification(notification)
+      }
+  }
+
+  public override func willPresent(_ notification: UNNotification, completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) -> Bool {
+    if EXScopedNotificationsUtils.shouldNotification(notification, beHandledByExperience: scopeKey) {
+      return super.willPresent(notification, completionHandler: completionHandler)
+    } else {
+      completionHandler([])
+    }
+    return true
+  }
+
+  public override func removeDeliveredNotifications(identifier: String) {
+    let scopedIdentifier = EXScopedNotificationsUtils.scopedIdentifier(fromId: identifier, forExperience: scopeKey)
+    UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: [scopedIdentifier])
+  }
+
+  public override func removeAllDeliveredNotifications() {
+    UNUserNotificationCenter.current().getDeliveredNotifications { notifications in
+      var identifiers: Set<String> = []
+      notifications.forEach {
+        if EXScopedNotificationsUtils.shouldNotification($0, beHandledByExperience: self.scopeKey) {
+          identifiers.insert($0.request.identifier)
+        }
+      }
+      UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: Array(identifiers))
+    }
+  }
+
+  public override func presentNotificationAsync(identifier: String, notificationSpec: [String: Any], promise: Promise) {
+    let scopedIdentifier = EXScopedNotificationsUtils.scopedIdentifier(fromId: identifier, forExperience: scopeKey)
+    super.presentNotificationAsync(identifier: identifier, notificationSpec: notificationSpec, promise: promise)
+  }
 }
