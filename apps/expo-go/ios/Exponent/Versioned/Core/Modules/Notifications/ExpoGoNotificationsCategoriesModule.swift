d263f9ce7efae67dc4b60feafadaa262d99b3274
Douglas Lowder <douglowder@mac.com>
2025-04-01 19:12:14 -0700

    Implement scoped categories module

--- apps/expo-go/ios/Exponent/Versioned/Core/Modules/Notifications/ExpoGoNotificationsCategoriesModule.swift
+++ apps/expo-go/ios/Exponent/Versioned/Core/Modules/Notifications/ExpoGoNotificationsCategoriesModule.swift
@@ -15,4 +15,25 @@ public final class ExpoGoNotificationsCategoriesModule: CategoriesModule {
 
     super.init(appContext: appContext)
   }
+
+  public override func getNotificationCategoriesAsync(promise: Promise) {
+    return getNotificationCategories { categoryRecords in
+      categoryRecords.forEach { record in
+        record.identifier = EXScopedNotificationsUtils.getUnscopedIdentifier(fromScopedIdentifier: record.identifier)
+      }
+      promise.resolve(categoryRecords)
+    } filter: { category in
+      return EXScopedNotificationsUtils.isId(category.identifier, scopedByExperience: self.scopeKey)
+    }
+  }
+
+  public override func setNotificationCategoryAsync(identifier: String, actions: [CategoryActionRecord], options: CategoryOptionsRecord?, promise: Promise) {
+    let scopedIdentifier = EXScopedNotificationsUtils.scopedIdentifier(fromId: identifier, forExperience: scopeKey)
+    super.setNotificationCategoryAsync(identifier: scopedIdentifier, actions: actions, options: options, promise: promise)
+  }
+
+  public override func deleteNotificationCategoryAsync(identifier: String, promise: Promise) {
+    let scopedIdentifier = EXScopedNotificationsUtils.scopedIdentifier(fromId: identifier, forExperience: scopeKey)
+    super.deleteNotificationCategoryAsync(identifier: scopedIdentifier, promise: promise)
+  }
 }
